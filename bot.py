48452684417ad47b5f4cb6e4873e3b2c4ad79f71
protokolsav-gif <protokol.sav@gmail.com>
2026-01-16 22:17:52 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import feedparser
+import requests
+import os
+from datetime import datetime, timedelta, timezone
+
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+RSS_FEEDS = [
+    "https://www.reuters.com/world/rss",
+    "https://www.politico.com/rss/politics08.xml",
+    "https://www.theguardian.com/world/rss",
+]
+
+KEYWORDS = [
+    "sanction", "arrest", "law", "ban", "court",
+    "investigation", "leak", "police", "government",
+    "election", "corruption
